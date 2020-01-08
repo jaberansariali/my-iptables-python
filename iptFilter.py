a869e01dbd3fc4c0da321e6845669feb1aa89293
root <a.ansari@fwutech.com>
2020-01-08 10:27:55 +0000

    my test

---
+++ iptFilter.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3.6
+##############################################################################################################################
+#########                                  WRITE BY A.ANSARI                                                                ##
+#########                                                                                                                   ##
+##############################################################################################################################
+##########Do not change any thing ###########################
+import iptc
+import sys
+############################################Discription###################################
+# This the input parameter  1#Ch:input/output  2#Po:tcp/udp/all  3#: interface/all 4#S:ip/subnet or all 5#D:ip/subnet or all 6#SP:portnumber/all 7#DP:portnumber/all 8#ACCEPT/DROP
+if len(sys.argv) > 9:
+    print('You have specified too many arguments')
+    sys.exit()
+
+if len(sys.argv) < 9:
+    print('You need to specify the path to be listed')
+    sys.exit()
+
+Chain = sys.argv[1]
+protocol = sys.argv[2]
+interface = sys.argv[3]
+source = sys.argv[4]
+destination = sys.argv[5]
+source_port = sys.argv[6]
+destination_port = sys.argv[7]
+targets = sys.argv[8]
+if Chain == "input":
+   Chain = "INPUT"
+elif Chain == "output":
+   Chain = "OUTPUT"
+if protocol == "all" or protocol == "ALL" :
+   protocol = ""
+
+
+
+#print(len(sys.argv))
+def drop():
+   # chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), Chain)
+   # rule = iptc.Rule()
+   # rule.protocol = protocol
+   # rule.in_interface = interface
+   # rule.add_match(match)
+   # match = iptc.Match(rule, "iprange")
+   # match.src_range = "192.168.1.100-192.168.1.200"
+   # match.dst_range = "172.22.33.106"
+   # rule.add_match(match)
+   # target = iptc.Target(rule, targets)
+   # rule.target = target
+   # chain.insert_rule(rule)
+   rule = iptc.Rule()
+   rule.protocol = protocol
+   match = iptc.Match(rule, protocol)
+   match.sport = source_port
+   match.dport = destination_port
+   rule.add_match(match)
+   rule.src = source
+   rule.dst = destination
+   rule.add_match(match)
+   rule.target = iptc.Target(rule, targets)
+   chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), Chain)
+   chain.insert_rule(rule)
+
+
+def allowLoopback():
+    chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+    rule = iptc.Rule()
+    rule.in_interface = "lo"
+    target = iptc.Target(rule, "ACCEPT")
+    rule.target = target
+    chain.insert_rule(rule)
+
+def allowEstablished():
+    chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
+    rule = iptc.Rule()
+    match = rule.create_match('state')
+    match.state = "RELATED,ESTABLISHED"
+    rule.target = iptc.Target(rule, 'ACCEPT')
+    chain.insert_rule(rule)
+
+drop()
+
+#allowLoopback()
+#allowEstablished()
+
+#print (sys.argv[1])
+#print (sys.argv[2])
---
+++ iptNAT.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3.6
+##############################################################################################################################
+#########                                  WRITE BY A.ANSARI                                                                ##
+#########                                                                                                                   ##
+##############################################################################################################################
+##########Do not change any thing ###########################
+import iptc
+class pop_table:
+    def __init__(self, table_name):
+        self.table = iptc.Table(table_name)
+        self.chains = dict()
+
+        for i in self.table.chains:
+            self.chains[i.name] = iptc.Chain(self.table, i.name)
+
+        self.method = {'append': self.append,
+                       'insert': self.insert}
+
+    def append(self, chain, rule):
+        tmp = self.chains[chain]
+        tmp.append_rule(rule)
+
+    def insert(self, chain, rule):
+        tmp = self.chains[chain]
+        tmp.insert_rule(rule)
+
+
+class make_rule(iptc.Rule):
+    def __init__(self):
+        iptc.Rule.__init__(self)
+
+        self.method={'block': self.block,
+                     'snat': self.snat,
+                     'allow': self.allow,
+                     'i_iface': self.i_iface,
+                     'o_iface': self.o_iface,
+                     'source': self.source,
+                     'destination': self.destination}
+
+    def block(self):
+        t = iptc.Target(self, 'REJECT')
+        self.target = t
+
+    def snat(self, snat_ip):
+        t = iptc.Target(self, 'SNAT')
+        t.to_source = snat_ip
+        self.target = t
+
+    def allow(self):
+        t = iptc.Target(self, 'ACCEPT')
+        self.target = t
+
+    def i_iface(self, iface):
+        self.in_interface = iface
+
+    def o_iface(self, iface):
+        self.out_interface = iface
+
+    def source(self, netaddr):
+        self.src = netaddr
+
+    def destination(self, netaddr):
+        self.dst = netaddr
+
+class phyawall:
+    def __init__(self):
+        self.list = []
+
+    def add_rule(self, rule_dict):
+        tbl = pop_table(rule_dict['tblchn']['table'])
+        chn = rule_dict['tblchn']['chain']
+        act = tbl.method[rule_dict['tblchn']['action']]
+        tmp = make_rule()
+
+        for i in rule_dict['rule']:
+            tmp.method[i](rule_dict['rule'][i])
+        act(chn, tmp)
+
+# Testing :: below will go into main app and the variables for change the chain 
+
+phyrule = dict()
+phyrule['tblchn'] = dict()
+phyrule['tblchn']['table'] = 'nat'
+phyrule['tblchn']['chain'] = 'POSTROUTING'
+phyrule['tblchn']['action'] = 'insert'
+phyrule['rule'] = dict()
+phyrule['rule']['o_iface'] = 'ens3'
+phyrule['rule']['snat'] = '110.1.2.250'
+phyrule['rule']['source'] = '110.9.60.9'
+phyrule['rule']['destination'] = '110.141.21.13'
+
+
+a = phyawall()
+a.add_rule(phyrule)
